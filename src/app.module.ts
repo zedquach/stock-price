b287e11d0c30915771a8f21f0c76e38c3b031176
Vu Quach <zed.quach@gmail.com>
2023-11-02 11:42:33 +0700

    feat: add price graphql model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PriceModule } from './price/price.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Interval } from './scalars';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      resolvers: { Interval: Interval },
+    }),
+    PriceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const INTERVALS = [
+  '1min',
+  '5min',
+  '15min',
+  '30min',
+  '60min',
+  'daily',
+  'weekly',
+  'monthly',
+];
